0f188277e198a9df614528b314e0d27ca48224eb
dangminh214 <dangminh214@gmail.com>
2025-11-10 09:15:43 +0700

    test run on zed

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from backend.models.model import load_llm_model
-from backend.utils import send_message_to_agent
-from backend.config import GROQ_KEY, OPENAI_API_KEY
-from backend.models.embedder import get_embedded
 import numpy as np
 
+from backend.config import GROQ_KEY, OPENAI_API_KEY
+from backend.models.embedder import get_embedded
+from backend.models.model import load_llm_model
+from backend.utils import send_message_to_agent
 
 assert GROQ_KEY, "Missing GROQ_KEY in .env file"
 
 
 def main():
-    agent = load_llm_model(
-        model="llama-3.1-8b-instant", 
-        key=GROQ_KEY
-    )
+    agent = load_llm_model(model="llama-3.1-8b-instant", key=GROQ_KEY)
 
-    texts = ["what is the weather today in sf ?", 
-             "how is the weather in San Francisco today?"]
-    
+    texts = [
+        "what is the weather today in sf ?",
+        "how is the weather in San Francisco today?",
+    ]
+
     embeddings = get_embedded(texts)
 
     # Cosine similarity
-    similarity = (np.dot(embeddings[0], embeddings[1]) / 
-                  (np.linalg.norm(embeddings[0]) * np.linalg.norm(embeddings[1])))
-    
+    similarity = np.dot(embeddings[0], embeddings[1]) / (
+        np.linalg.norm(embeddings[0]) * np.linalg.norm(embeddings[1])
+    )
+
     print("Embedding vector size:", len(embeddings[0]))
     print("Cosine similarity between texts:", similarity)
 
@@ -30,11 +30,8 @@ def main():
         response = send_message_to_agent(agent, prompt=prompt)
 
         # Accesss the main data
-        print(response["messages"][2].content) 
+        print(response["messages"][2].content)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
-
-
