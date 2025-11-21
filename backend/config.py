cba3ad655eff38199895b147dd3254e1e6602154
dangminh214 <dangminh214@gmail.com>
2025-11-21 23:11:50 +0700

    update requirements

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,4 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_KEY")
-GROQ_KEY = os.getenv("GROQ_KEY") # I use free model Groq
+GROQ_KEY = os.getenv("GROQ_KEY") #  use free model Groq
